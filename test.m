d6e722ebf4cbca137cee744b4880c7cac4bba75e
Marco Zhang <marcozhang703@outlook.com>
2020-01-10 16:33:23 +0800

    Create New Repo

---
+++ test.m
@@ -0,0 +1,15 @@
+% load MapData.mat;
+% 
+% figure(1);
+% imshow(Final_Data);
+% 
+% figure(2);
+% imshow(CLOSED);
+% 
+% figure(3);
+% imshow(Display_Data);
+% 
+% figure(4)
+% imshow(MAP);
+
+imread('depthToBgra1.jpg');
