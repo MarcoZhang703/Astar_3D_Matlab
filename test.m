184cfcb321fc0a5c3e6981496d9045125e3fb2d8
Marco Zhang <marcozhang703@outlook.com>
2020-01-11 22:44:50 +0800

    Update New PLY File

--- test.m
+++ test.m
@@ -13,18 +13,22 @@
 % imshow(MAP);
 
 %imread('depthToBgra1.jpg');
-clc
-clear
-ptCloud = pcread('./damn/5.pcd');
+% clc
+% clear
+% ptCloud = pcread('./damn/5.pcd');
+% 
+% figure(1)
+% pcshow(ptCloud);
+% grid on;
+% 
+% figure(2);
+% pcshow('1.ply');
+% title('PLY Point Cloud')
+% grid on;
+% 
+% pcwrite(ptCloud, './damn/5.ply');
+pcread('1_process.ply');
 
-figure(1)
-pcshow(ptCloud);
+figure(1);
+pcshow('1_process.ply');
 grid on;
-
-figure(2);
-pcshow('1.ply');
-title('PLY Point Cloud')
-grid on;
-
-pcwrite(ptCloud, './damn/5.ply');
-
